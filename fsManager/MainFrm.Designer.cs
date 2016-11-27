840aaf71e3390defc75e70a8985b918e111f48cc
mguinzbourg <mguinzbourg@MG_AT_WORK>
2016-11-27 11:03:58 -0500

    mmm

--- fsManager/MainFrm.Designer.cs
+++ fsManager/MainFrm.Designer.cs
@@ -28,36 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SkatersGrid = new System.Windows.Forms.DataGridView();
+            this.buttonAll = new System.Windows.Forms.Button();
+            this.buttonUnlimited = new System.Windows.Forms.Button();
+            this.buttonStarskate = new System.Windows.Forms.Button();
+            this.buttonLTS = new System.Windows.Forms.Button();
+            this.buttonProfile = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.SkatersGrid)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // SkatersGrid
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(1040, 857);
-            this.dataGridView1.TabIndex = 0;
+            this.SkatersGrid.AllowUserToAddRows = false;
+            this.SkatersGrid.AllowUserToDeleteRows = false;
+            this.SkatersGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SkatersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SkatersGrid.Location = new System.Drawing.Point(0, 0);
+            this.SkatersGrid.Name = "SkatersGrid";
+            this.SkatersGrid.ReadOnly = true;
+            this.SkatersGrid.RowHeadersVisible = false;
+            this.SkatersGrid.RowTemplate.Height = 28;
+            this.SkatersGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.SkatersGrid.Size = new System.Drawing.Size(1172, 750);
+            this.SkatersGrid.TabIndex = 0;
+            // 
+            // buttonAll
+            // 
+            this.buttonAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAll.Location = new System.Drawing.Point(1178, 8);
+            this.buttonAll.Name = "buttonAll";
+            this.buttonAll.Size = new System.Drawing.Size(142, 36);
+            this.buttonAll.TabIndex = 1;
+            this.buttonAll.Text = "All skaters";
+            this.buttonAll.UseVisualStyleBackColor = true;
+            this.buttonAll.Click += new System.EventHandler(this.buttonAll_Click);
+            // 
+            // buttonUnlimited
+            // 
+            this.buttonUnlimited.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUnlimited.Location = new System.Drawing.Point(1178, 50);
+            this.buttonUnlimited.Name = "buttonUnlimited";
+            this.buttonUnlimited.Size = new System.Drawing.Size(142, 36);
+            this.buttonUnlimited.TabIndex = 2;
+            this.buttonUnlimited.Text = "Unlimited";
+            this.buttonUnlimited.UseVisualStyleBackColor = true;
+            this.buttonUnlimited.Click += new System.EventHandler(this.buttonUnlimited_Click);
+            // 
+            // buttonStarskate
+            // 
+            this.buttonStarskate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStarskate.Location = new System.Drawing.Point(1178, 92);
+            this.buttonStarskate.Name = "buttonStarskate";
+            this.buttonStarskate.Size = new System.Drawing.Size(142, 36);
+            this.buttonStarskate.TabIndex = 3;
+            this.buttonStarskate.Text = "Star Skate";
+            this.buttonStarskate.UseVisualStyleBackColor = true;
+            this.buttonStarskate.Click += new System.EventHandler(this.buttonStarskate_Click);
+            // 
+            // buttonLTS
+            // 
+            this.buttonLTS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLTS.Location = new System.Drawing.Point(1178, 134);
+            this.buttonLTS.Name = "buttonLTS";
+            this.buttonLTS.Size = new System.Drawing.Size(142, 36);
+            this.buttonLTS.TabIndex = 4;
+            this.buttonLTS.Text = "Learn To Skate";
+            this.buttonLTS.UseVisualStyleBackColor = true;
+            this.buttonLTS.Click += new System.EventHandler(this.buttonLTS_Click);
+            // 
+            // buttonProfile
+            // 
+            this.buttonProfile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonProfile.Location = new System.Drawing.Point(1178, 176);
+            this.buttonProfile.Name = "buttonProfile";
+            this.buttonProfile.Size = new System.Drawing.Size(142, 36);
+            this.buttonProfile.TabIndex = 5;
+            this.buttonProfile.Text = "Profile";
+            this.buttonProfile.UseVisualStyleBackColor = true;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.Location = new System.Drawing.Point(1178, 702);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(142, 36);
+            this.buttonClose.TabIndex = 6;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
             // 
             // MainFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1199, 857);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(1331, 750);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonProfile);
+            this.Controls.Add(this.buttonLTS);
+            this.Controls.Add(this.buttonStarskate);
+            this.Controls.Add(this.buttonUnlimited);
+            this.Controls.Add(this.buttonAll);
+            this.Controls.Add(this.SkatersGrid);
             this.Name = "MainFrm";
             this.Text = "Skaters";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SkatersGrid)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView SkatersGrid;
+        private System.Windows.Forms.Button buttonAll;
+        private System.Windows.Forms.Button buttonUnlimited;
+        private System.Windows.Forms.Button buttonStarskate;
+        private System.Windows.Forms.Button buttonLTS;
+        private System.Windows.Forms.Button buttonProfile;
+        private System.Windows.Forms.Button buttonClose;
     }
 }
 
--- fsManager/MainFrm.cs
+++ fsManager/MainFrm.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,138 @@ namespace fsManager
 {
     public partial class MainFrm : Form
     {
+        SQLiteConnection m_dbConnection;
         public MainFrm()
         {
             InitializeComponent();
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(Application.StartupPath + "\\rtc.db"))
+            {
+                MessageBox.Show("skaters db file not found");
+                Close();
+                return;
+            }
+
+            m_dbConnection = new SQLiteConnection("Data Source=rtc.db;Version=3;");
+            m_dbConnection.Open();
+
+            showAllSkaters();
+
+            ResizeColumns();
+        }
+
+        private void ResizeColumns()
+        {
+            for (int x = 0; x < SkatersGrid.ColumnCount; x++)
+            {
+                SkatersGrid.Columns[x].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            }
+
+            SkatersGrid.Columns[SkatersGrid.ColumnCount - 1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        private void showAllSkaters()
+        {
+            string SQL = "SELECT FirstName, LastName, DOB, Sex, Phone, Email, ParentName, SkateCanadaN, CoachName, LevelRegistered, LTSDay, Notes FROM skaters";
+            SQLiteCommand cmd = new SQLiteCommand(SQL, m_dbConnection);
+            SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            try
+            {
+                da.Fill(ds);
+                DataTable dt = ds.Tables[0];
+                SkatersGrid.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void showUnlimitedSkaters()
+        {
+            string SQL = "SELECT FirstName, LastName, DOB, Sex, Phone, Email, ParentName, SkateCanadaN, CoachName, LevelRegistered, LTSDay, Notes FROM skaters WHERE LevelRegistered like '%unlimited%'";
+            SQLiteCommand cmd = new SQLiteCommand(SQL, m_dbConnection);
+            SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            try
+            {
+                da.Fill(ds);
+                DataTable dt = ds.Tables[0];
+                SkatersGrid.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void showLTSSkaters()
+        {
+            string SQL = "SELECT FirstName, LastName, DOB, Sex, Phone, Email, ParentName, SkateCanadaN, CoachName, LevelRegistered, LTSDay, Notes FROM skaters WHERE LevelRegistered like '%learn%'";
+            SQLiteCommand cmd = new SQLiteCommand(SQL, m_dbConnection);
+            SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            try
+            {
+                da.Fill(ds);
+                DataTable dt = ds.Tables[0];
+                SkatersGrid.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void showStarskateSkaters()
+        {
+            string SQL = "SELECT FirstName, LastName, DOB, Sex, Phone, Email, ParentName, SkateCanadaN, CoachName, LevelRegistered, LTSDay, Notes FROM skaters WHERE LevelRegistered like '%starskate%'";
+            SQLiteCommand cmd = new SQLiteCommand(SQL, m_dbConnection);
+            SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            try
+            {
+                da.Fill(ds);
+                DataTable dt = ds.Tables[0];
+                SkatersGrid.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonAll_Click(object sender, EventArgs e)
+        {
+            showAllSkaters();
+        }
+
+        private void buttonUnlimited_Click(object sender, EventArgs e)
+        {
+            showUnlimitedSkaters();
+        }
+
+        private void buttonStarskate_Click(object sender, EventArgs e)
+        {
+            showStarskateSkaters();
+        }
+
+        private void buttonLTS_Click(object sender, EventArgs e)
+        {
+            showLTSSkaters();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
